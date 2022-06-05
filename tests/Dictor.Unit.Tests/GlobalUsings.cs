e1c9cf95fdf0dd06ce1e7bd70190934bf9cd40b6
UserIsntAvailable <PinedaP.Angel@gmail.com>
2022-06-04 23:30:21 -0400

    Add base project

---
+++ tests/Dictor.Unit.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using Microsoft.CodeAnalysis;
+global using Microsoft.CodeAnalysis.CSharp;
+global using Microsoft.CodeAnalysis.CSharp.Testing;
+global using Microsoft.CodeAnalysis.Testing.Verifiers;
+global using Xunit;
